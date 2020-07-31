70ed14d47f2a5e9b52afd358b238ce439950768d
Jorge Morales <manriquem1901@gmail.com>
2020-07-31 17:47:45 -0500

    Files for the solution of the n queens problem were added.

---
+++ Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_backtracking.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // TODO: implement solution with backtracking
+    return 0;
+}
---
+++ Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_brute_force.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // TODO: implement brute force solution
+    return 0;
+}
