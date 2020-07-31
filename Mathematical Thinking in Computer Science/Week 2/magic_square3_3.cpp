3da0d049b6b1d0f96dc6693c52353d3c6b071124
Jorge Morales <manriquem1901@gmail.com>
2020-07-31 13:02:55 -0500

    magic_square3_3.cpp finished.

--- Mathematical Thinking in Computer Science/Week 2/magic_square3_3.cpp
+++ Mathematical Thinking in Computer Science/Week 2/magic_square3_3.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Brute force search
 bool check_square(const vector<int>& permutation) {
     int n = permutation.size();
     int sum = 0;
@@ -72,9 +73,88 @@ void magic_square() {
 void method_two() {
     // TODO: implement
     int goal = 15;
+    int matrix[3][3];
+
+    // 5 should be in the middle
+    matrix[1][1] = 5;
+
+    vector<pair<int, int>> pos_one = {{0, 1}, {1, 0}, {1, 2}, {2, 1}};
+
+    for (int i = 0; i < 4; i++) {
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                matrix[k][l] = -1;
+            }
+        }
+        matrix[1][1] = 5;
+        matrix[pos_one[i].first][pos_one[i].second] = 1;
+        for (int j = 0; j < 2; j++) {
+            if (j == 0) {
+                if (pos_one[i].first == 0 || pos_one[i].first == 2) {
+                    matrix[pos_one[i].first][pos_one[i].second - 1] = 6;
+                    matrix[pos_one[i].first][pos_one[i].second + 1] = 8;
+                } else {
+                    matrix[pos_one[i].first - 1][pos_one[i].second] = 6;
+                    matrix[pos_one[i].first + 1][pos_one[i].second] = 8;
+                }
+            } else {
+                if (pos_one[i].first == 0 || pos_one[i].first == 2) {
+                    matrix[pos_one[i].first][pos_one[i].second - 1] = 8;
+                    matrix[pos_one[i].first][pos_one[i].second + 1] = 6;
+                } else {
+                    matrix[pos_one[i].first - 1][pos_one[i].second] = 8;
+                    matrix[pos_one[i].first + 1][pos_one[i].second] = 6;
+                }
+            }
+            vector<pair<int, int>> pendent;
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+                    if (matrix[k][l] == -1) {
+                        if (k == 0 && l == 0) {
+                            matrix[k][l] = goal - (5 + matrix[2][2]);
+                        } else if (k == 0 && l == 2) {
+                            matrix[k][l] = goal - (5 + matrix[2][0]);
+                        } else if (k == 2 && l == 0) {
+                            matrix[k][l] = goal - (5 + matrix[0][2]);
+                        } else if (k == 2 && l == 2) {
+                            matrix[k][l] = goal - (5 + matrix[0][0]);
+                        } else {
+                            if (k == 0 && matrix[2][1] != -1) {
+                                matrix[k][l] = goal - (matrix[2][1] + 5);
+                            } else if (k == 1 && l == 0 && matrix[1][2] != -1) {
+                                matrix[k][l] = goal - (matrix[1][2] + 5);
+                            } else if (k == 1 && l == 2 && matrix[1][0] != -1) {
+                                matrix[k][l] = goal - (matrix[1][0] + 5);
+                            } else if(k == 2 && matrix[0][1] != -1) {
+                                matrix[k][l] = goal - (matrix[0][1] + 5);
+                            } else {
+                                pendent.push_back(make_pair(k, l));
+                            }
+                        }
+                    }
+                }
+            }
+            for (auto p: pendent) {
+                if (p.first == 0 || p.first == 2) {
+                    matrix[p.first][p.second] = goal - (matrix[p.first][p.second - 1] + matrix[p.first][p.second + 1]);
+                } else {
+                    matrix[p.first][p.second] = goal - (matrix[p.first - 1][p.second] + matrix[p.first + 1][p.second]);
+                }
+            }
+
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+                    cout << matrix[k][l] << " ";
+                }
+                cout << "\n";
+            }
+            cout << "----------------------------------\n";
+        }
+    }
 }
 
 int main() {
     magic_square();
+    method_two();
     return 0;
 }
