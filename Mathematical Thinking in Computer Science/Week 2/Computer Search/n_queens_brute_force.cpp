4be775a6c06ff7ca35d0417598bd12bf0c5f5790
Jorge Morales <manriquem1901@gmail.com>
2020-08-01 14:09:09 -0500

    Brute force solution of the n queens problem in c++ was added.

--- Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_brute_force.cpp
+++ Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_brute_force.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+bool is_solution(const vector<int>& permutation) {
+    int n = permutation.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (abs(i - j) == abs(permutation[i] - permutation[j]) && i != j)
+                return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     // TODO: implement brute force solution
+    int n = 4;
+    vector<int> permutation;
+    for (int i = 0; i < n; i++) {
+        permutation.push_back(i);
+    }
+    do {
+        if (is_solution(permutation)) {
+            for (int i = 0; i < n; i++) {
+                cout << permutation[i] << " ";
+            }
+            cout << "\n";
+        }
+    } while(next_permutation(permutation.begin(), permutation.end()));
     return 0;
 }
