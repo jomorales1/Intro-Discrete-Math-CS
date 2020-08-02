8aee51c84ebc9bb15e94fdba155a026ea29962af
Jorge Morales <manriquem1901@gmail.com>
2020-08-01 21:38:03 -0500

    Solution to the n queens problem implemented in c++ was added.

--- Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_backtracking.cpp
+++ Mathematical Thinking in Computer Science/Week 2/Computer Search/n_queens_backtracking.cpp
@@ -2,7 +2,66 @@
 
 using namespace std;
 
+bool contains(const vector<int>& v, int el) {
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el)
+            return true;
+    }
+    return false;
+}
+
+bool can_be_extended_to_solution(const vector<int>& perm) {
+    int i = perm.size() - 1;
+    for (int j = 0; j < i; j++) {
+        if ((i - j) == abs(perm[i] - perm[j]))
+            return false;
+    }
+    return true;
+}
+
+void generate_permutations(vector<int>& perm, int n) {
+    if (perm.size() == n) {
+        cout << "[";
+        for (int i = 0; i < n - 1; i++) {
+            cout << perm[i] << ", ";
+        }
+        cout << perm.back() << "]\n";
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (!contains(perm, i)) {
+            perm.push_back(i);
+            generate_permutations(perm, n);
+            perm.pop_back();
+        }
+    }
+}
+
+void extend(vector<int>& perm, int n) {
+    if (perm.size() == n) {
+        cout << "[";
+        for (int i = 0; i < n - 1; i++) {
+            cout << perm[i] << ", ";
+        }
+        cout << perm.back() << "]\n";
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (!contains(perm, i)) {
+            perm.push_back(i);
+            if (can_be_extended_to_solution(perm))
+                extend(perm, n);
+            perm.pop_back();
+        }
+    }
+}
+
 int main() {
     // TODO: implement solution with backtracking
+    vector<int> perm;
+    int n;
+    cin >> n;
+    extend(perm, n);
     return 0;
 }
