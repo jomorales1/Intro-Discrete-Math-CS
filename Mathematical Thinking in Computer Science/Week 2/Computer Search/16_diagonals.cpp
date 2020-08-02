26035c30dd775eb401c6fe39dbe1d8c809af236e
Jorge Morales <manriquem1901@gmail.com>
2020-08-01 22:08:02 -0500

    Template for the solution of the 16 diagonals in c++ was added.

---
+++ Mathematical Thinking in Computer Science/Week 2/Computer Search/16_diagonals.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool check() {
+    return true;
+}
+
+void backtrack() {
+    
+}
+
+int main() {
+    // TODO: Implement the solution using the backtrack technique
+    return 0;
+}
